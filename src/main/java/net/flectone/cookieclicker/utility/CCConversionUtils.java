7092f4bf7bc8464393e2862b02f9fbf89e1ef108
FunnyBars <realepibarsic@gmail.com>
2025-03-16 00:12:33 +0300

    add NMS and PacketEvents itemstacks conversion

--- src/main/java/net/flectone/cookieclicker/utility/CCConversionUtils.java
+++ src/main/java/net/flectone/cookieclicker/utility/CCConversionUtils.java
@@ -1,13 +1,19 @@
 package net.flectone.cookieclicker.utility;
 
 import com.github.retrooper.packetevents.protocol.player.User;
+import com.github.retrooper.packetevents.wrapper.PacketWrapper;
 import com.google.inject.Singleton;
 import com.mojang.serialization.JavaOps;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.papermc.paper.adventure.WrapperAwareSerializer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.ComponentSerializer;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 
 @Singleton
 public class CCConversionUtils {
@@ -27,5 +33,27 @@ public class CCConversionUtils {
                 player = i;
         }
         return player;
+    }
+
+    public ItemStack toMinecraftStack(com.github.retrooper.packetevents.protocol.item.ItemStack stack, RegistryAccess registries) {
+        ByteBuf buf = PooledByteBufAllocator.DEFAULT.buffer();
+        try {
+            PacketWrapper.createUniversalPacketWrapper(buf).writeItemStack(stack);
+            return net.minecraft.world.item.ItemStack.OPTIONAL_STREAM_CODEC.decode(
+                    new RegistryFriendlyByteBuf(buf, registries));
+        } finally {
+            buf.release();
+        }
+    }
+
+    public com.github.retrooper.packetevents.protocol.item.ItemStack fromMinecraftStack(ItemStack stack, RegistryAccess registries) {
+        ByteBuf buf = PooledByteBufAllocator.DEFAULT.buffer();
+        try {
+            net.minecraft.world.item.ItemStack.OPTIONAL_STREAM_CODEC.encode(
+                    new RegistryFriendlyByteBuf(buf, registries), stack);
+            return PacketWrapper.createUniversalPacketWrapper(buf).readItemStack();
+        } finally {
+            buf.release();
+        }
     }
 }
