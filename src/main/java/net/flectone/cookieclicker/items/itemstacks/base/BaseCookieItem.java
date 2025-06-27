35d7829cb212be3b6936b8369c9e6ef1c715c36a
FunnyBars <realepibarsic@gmail.com>
2025-06-28 02:07:23 +0300

    add Minecraft ItemStack to CookieClicker conversion

--- src/main/java/net/flectone/cookieclicker/items/itemstacks/base/BaseCookieItem.java
+++ src/main/java/net/flectone/cookieclicker/items/itemstacks/base/BaseCookieItem.java
@@ -1,13 +1,26 @@
 package net.flectone.cookieclicker.items.itemstacks.base;
 
+import com.mojang.serialization.JavaOps;
+import io.papermc.paper.adventure.WrapperAwareSerializer;
 import net.flectone.cookieclicker.items.attributes.CookieAbility;
 import net.flectone.cookieclicker.items.attributes.ToolType;
 import net.flectone.cookieclicker.items.itemstacks.base.data.Features;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.ComponentSerializer;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.item.component.ItemLore;
+import org.jetbrains.annotations.ApiStatus;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class BaseCookieItem extends CookieItemStack {
 
@@ -16,6 +29,80 @@ public class BaseCookieItem extends CookieItemStack {
 
         if (features.getCategory() != ToolType.NONE) {
             removeVisibleAttributes(true);
+        }
+    }
+
+    @ApiStatus.Experimental
+    public static BaseCookieItem fromItemStack(ItemStack itemStack) {
+        CustomData customData = itemStack.getComponents().get(DataComponents.CUSTOM_DATA);
+        CompoundTag compoundTag = customData != null ? customData.copyTag() : new CompoundTag();
+        CompoundTag cookieClickerTag = compoundTag.contains(CookieItems.PLUGIN_KEY)
+                ? compoundTag.getCompound(CookieItems.PLUGIN_KEY)
+                : new CompoundTag();
+
+        //конвертация старых предметов
+        cookieClickerTag = compoundTag.contains("cookies")
+                ? compoundTag.getCompound("cookies")
+                : cookieClickerTag;
+        //
+
+        BaseCookieItem customItem = new BaseCookieItem(itemStack.getItem(), new Features(cookieClickerTag));
+
+        itemStack.getComponents().forEach(customItem::applyComponent);
+
+        ItemLore itemLore = itemStack.getComponents().has(DataComponents.LORE) ? itemStack.getComponents().get(DataComponents.LORE) : null;
+        if (itemLore != null && !itemLore.lines().isEmpty()) {
+            extractDescription(itemLore).forEach(customItem::addLore);
+        }
+
+        if (compoundTag.contains("cookies")) {
+            customItem.fixOldItem();
+        }
+
+        return customItem;
+    }
+
+    //хз как нормально это сделать, ну вот
+    private static List<String> extractDescription(ItemLore itemLore) {
+        List<String> description = new ArrayList<>();
+        String singleLine;
+
+        List<net.minecraft.network.chat.Component> lore = new ArrayList<>(itemLore.lines());
+        lore.removeFirst(); //Убираем первую строку, в которой "#предмет"
+
+        //Штука, которая переделывает nms компонент в adventure
+        ComponentSerializer<Component, Component, net.minecraft.network.chat.Component> componentSerializer;
+        componentSerializer = new WrapperAwareSerializer(() -> MinecraftServer.getServer().registryAccess().createSerializationContext(JavaOps.INSTANCE));
+
+        if (lore.isEmpty())
+            return description;
+
+        for (net.minecraft.network.chat.Component component : lore) {
+            singleLine = miniMessage.serialize(componentSerializer.deserialize(component));
+            //Если строка "Когда используется/в ведущей руке", то значит дальше статы и их вытаскивать не надо
+            //При сборке предмета перед статами добавляется пустая строка, поэтому тут её надо будет убрать
+            if (singleLine.contains("Когда")) {
+                break;
+            }
+
+            description.add(singleLine);
+        }
+
+        description.removeIf(string -> description.indexOf(string) == description.size() - 1 && string.isEmpty());
+
+        return description;
+    }
+
+    private void fixOldItem() {
+        if (originalMaterial == Items.LEATHER_HORSE_ARMOR) {
+            removeComponent(DataComponents.EQUIPPABLE);
+            removeComponent(DataComponents.ITEM_NAME);
+            removeComponent(DataComponents.DYED_COLOR);
+            originalMaterial = HIDDEN_ITEM;
+
+            //У меня пока что предметы становятся mojang_banner_pattern,
+            //а у него есть редкость
+            removeComponent(DataComponents.RARITY);
         }
     }
 
