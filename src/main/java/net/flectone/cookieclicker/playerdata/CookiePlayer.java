bb26cc9a125d64fd32514f352a188baf5f29e547
FunnyBars <realepibarsic@gmail.com>
2025-06-28 21:38:59 +0300

    update CookiePlayer

---
+++ src/main/java/net/flectone/cookieclicker/playerdata/CookiePlayer.java
@@ -0,0 +1,28 @@
+package net.flectone.cookieclicker.playerdata;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+public class CookiePlayer {
+    protected final UUID uuid;
+    @Setter
+    protected int iFrameClicks = 0;
+    @Setter
+    protected int remainingXp = 1500;
+    @Setter
+    protected int lvl = 0;
+
+    public CookiePlayer(UUID uuid, int iFrameClicks, int remainingXp, int lvl) {
+        this.uuid = uuid;
+        this.iFrameClicks = iFrameClicks;
+        this.remainingXp = remainingXp;
+        this.lvl = lvl;
+    }
+
+    public CookiePlayer(UUID uuid) {
+        this.uuid = uuid;
+    }
+}
---
+++ src/main/java/net/flectone/cookieclicker/playerdata/ServerCookiePlayer.java
@@ -0,0 +1,86 @@
+package net.flectone.cookieclicker.playerdata;
+
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.protocol.player.User;
+import com.github.retrooper.packetevents.wrapper.PacketWrapper;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityAnimation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.player.Player;
+
+import java.util.UUID;
+
+public class ServerCookiePlayer extends CookiePlayer {
+    public ServerCookiePlayer(UUID uuid, int iFrameClicks, int remainXp, int lvl) {
+        super(uuid, iFrameClicks, remainXp, lvl);
+    }
+
+    public ServerCookiePlayer(UUID uuid) {
+        super(uuid);
+    }
+
+    public void addXp(Integer amount) {
+        int currentXP = remainingXp - amount;
+        int newXp;
+
+        while (currentXP <= 0) {
+            lvl++;
+            newXp = (int) ((Math.log(lvl)/Math.log(1.1d)) + 20 * lvl + 1500);
+            currentXP = newXp - Math.abs(currentXP);
+        }
+        remainingXp = currentXP;
+    }
+
+    private User getUser(UUID uuid) {
+        Object channel = PacketEvents.getAPI().getProtocolManager().getChannel(uuid);
+        if (channel == null) return null;
+
+        return PacketEvents.getAPI().getProtocolManager().getUser(channel);
+    }
+
+    public User getUser() {
+        return getUser(uuid);
+    }
+
+    private Player getPlayer(UUID uuid) {
+        //короче, тут я беру всех игроков на сервере и с помощью UUID нахожу нужного
+        Player player = null;
+        for (Player i : MinecraftServer.getServer().getPlayerList().players) {
+            if (i.getUUID().equals(uuid))
+                player = i;
+        }
+        return player;
+    }
+
+    public Player getPlayer() {
+        return getPlayer(uuid);
+    }
+
+    public Integer getId() {
+        return getUser().getEntityId();
+    }
+
+    public void sendPEpacket(PacketWrapper<?> packetWrapper, boolean silent) {
+        if (silent)
+            PacketEvents.getAPI().getProtocolManager().sendPacketSilently(
+                    PacketEvents.getAPI().getProtocolManager().getChannel(uuid),
+                    packetWrapper
+            );
+        else
+            PacketEvents.getAPI().getProtocolManager().sendPacket(
+                    PacketEvents.getAPI().getProtocolManager().getChannel(uuid),
+                    packetWrapper
+            );
+    }
+
+    public void sendPEpacket(PacketWrapper<?> packetWrapper) {
+        sendPEpacket(packetWrapper, false);
+    }
+
+    public void swingArm() {
+        WrapperPlayServerEntityAnimation animation = new WrapperPlayServerEntityAnimation(
+                getId(),
+                WrapperPlayServerEntityAnimation.EntityAnimationType.SWING_MAIN_ARM
+        );
+        sendPEpacket(animation);
+    }
+}
