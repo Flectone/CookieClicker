2dea6ad429643eedc0931eafc33590ee1b6eb246
FunnyBars <realepibarsic@gmail.com>
2025-07-05 15:08:47 +0300

    update and rename LegendaryHoeUpgrade

--- src/main/java/net/flectone/cookieclicker/cookiePart/LegendaryHoeUpgrade.java
+++ src/main/java/net/flectone/cookieclicker/cookiePart/LegendaryHoeUpgrade.java
@@ -10,11 +10,10 @@ import com.github.retrooper.packetevents.protocol.sound.Sounds;
 import com.github.retrooper.packetevents.util.Vector3d;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import net.flectone.cookieclicker.utility.PacketUtils;
-import net.flectone.cookieclicker.utility.CCConversionUtils;
-import net.flectone.cookieclicker.utility.CCobjects.Items.ClickerItems;
-import net.flectone.cookieclicker.utility.ItemTagsUtility;
-import net.flectone.cookieclicker.utility.UtilsCookie;
+import net.flectone.cookieclicker.items.attributes.CookieAbility;
+import net.flectone.cookieclicker.items.attributes.StatType;
+import net.flectone.cookieclicker.items.itemstacks.GeneratedCookieItem;
+import net.flectone.cookieclicker.utility.*;
 import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.ResourceLocation;
@@ -26,20 +25,22 @@ import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.phys.HitResult;
 
 @Singleton
-public class BagHoeUpgrade {
+public class LegendaryHoeUpgrade {
     private final UtilsCookie utilsCookie;
     private final ItemTagsUtility itemTagsUtility;
     private final PacketUtils packetUtils;
     private final CCConversionUtils conversionUtils;
+    private final StatsUtils statsUtils;
 
 
     @Inject
-    public BagHoeUpgrade(UtilsCookie utilsCookie, ItemTagsUtility itemTagsUtility, PacketUtils packetUtils,
-                         CCConversionUtils conversionUtils) {
+    public LegendaryHoeUpgrade(UtilsCookie utilsCookie, ItemTagsUtility itemTagsUtility, PacketUtils packetUtils,
+                               CCConversionUtils conversionUtils, StatsUtils statsUtils) {
         this.utilsCookie = utilsCookie;
         this.itemTagsUtility = itemTagsUtility;
         this.packetUtils = packetUtils;
         this.conversionUtils = conversionUtils;
+        this.statsUtils = statsUtils;
     }
 
     public boolean updateHoe(User user) {
@@ -48,12 +49,10 @@ public class BagHoeUpgrade {
 
         if (itemInHand.getItem().equals(Items.AIR)) return false;
 
-        String ability = itemTagsUtility.getAbility(itemInHand);
-        if (!(ability.equals("infinity"))) return false;
+        CookieAbility ability = statsUtils.getAbility(itemInHand);
+        if (ability != CookieAbility.INFINITY_UPGRADE) return false;
 
-        //itemNMS.getComponents().get(DataComponents.DAMAGE) может выдать null и это плохо
-        //Но теперь оно не может выдать null и это хорошо
-        //200iq момент
+        //получаем текущую прочность
         Object currentDamage = itemInHand.getComponents().get(DataComponents.DAMAGE);
         int damage = currentDamage == null ? 0 : (int) currentDamage;
 
@@ -62,13 +61,12 @@ public class BagHoeUpgrade {
         if (damage <= 1) {
             setDamage(itemInHand, 99);
 
-            itemTagsUtility.setStat(itemInHand, ClickerItems.fortuneTag, itemTagsUtility.getBaseFortune(itemInHand) + 1);
+            GeneratedCookieItem upgradeableItem = GeneratedCookieItem.fromItemStack(itemInHand);
+            upgradeableItem.addStat(StatType.FARMING_FORTUNE, 1);
+            player.setItemInHand(InteractionHand.MAIN_HAND, upgradeableItem.toNMS());
         }
         packetUtils.playSound(user, Sounds.BLOCK_NETHERITE_BLOCK_BREAK, 1f, 1.8f);
 
-        //кринж штука, чисто для теста
-        player.setItemInHand(InteractionHand.MAIN_HAND, itemInHand);
-        utilsCookie.updateStats(player.getItemInHand(InteractionHand.MAIN_HAND));
         return true;
     }
 
@@ -80,7 +78,7 @@ public class BagHoeUpgrade {
         );
     }
 
-    public void LegHoeChange (User user) {
+    public void legHoeChange (User user) {
         Player player = conversionUtils.userToNMS(user);
 
         //проверка, клик в воздух или нет
@@ -89,16 +87,28 @@ public class BagHoeUpgrade {
 
         //предмет в руке
         ItemStack itemInHand = player.getItemInHand(InteractionHand.MAIN_HAND);
-        if (!(itemTagsUtility.getItemTag(itemInHand).equals("leg_hoe"))) return;
+        if (!statsUtils.getItemTag(itemInHand).equals("leg_hoe")) return;
 
-        //компонент item_model не берёт просто Item, ему надо ResourceLocation, поэтому вот
+        GeneratedCookieItem legendaryHoe = GeneratedCookieItem.fromItemStack(itemInHand);
+
+        //Текущий режим предмета
+        //Мне лень делать это как-то по-другому,
+        //поэтому я просто беру из nms предмета модель
         ResourceLocation resourceLocation = itemInHand.getComponents().get(DataComponents.ITEM_MODEL);
-        if (resourceLocation == null) return;
+        String currentMode = resourceLocation != null
+                ? resourceLocation.toString()
+                : "none";
 
-        String newCurrentItemType = resourceLocation.toString().equals("minecraft:golden_hoe") ? "iron_hoe" : "golden_hoe";
-        DataComponentPatch itemModelComponent = DataComponentPatch.builder()
-                .set(DataComponents.ITEM_MODEL, ResourceLocation.tryBuild(ResourceLocation.DEFAULT_NAMESPACE, newCurrentItemType))
-                .build();
+
+        legendaryHoe.setItemModel(currentMode.equals("minecraft:golden_hoe")
+                ? Items.IRON_HOE
+                : Items.GOLDEN_HOE);
+
+        legendaryHoe.setAbility(currentMode.equals("minecraft:golden_hoe")
+                ? CookieAbility.TRANSFORM
+                : CookieAbility.INFINITY_UPGRADE);
+
+        player.setItemInHand(InteractionHand.MAIN_HAND, legendaryHoe.toNMS());
 
         //частицы красивые
         ParticleDustColorTransitionData particleDustColorTransitionData = new ParticleDustColorTransitionData(1f,
@@ -106,8 +116,6 @@ public class BagHoeUpgrade {
         Particle<?> particle = new Particle<>(ParticleTypes.DUST_COLOR_TRANSITION, particleDustColorTransitionData);
 
         packetUtils.spawnParticle(user, particle, 50, new Vector3d(player.getX(), player.getY() + 1, player.getZ()), 0.5f);
-        player.getItemInHand(InteractionHand.MAIN_HAND).applyComponents(itemModelComponent);
-        itemTagsUtility.setAbility(itemInHand, resourceLocation.toString().equals("minecraft:golden_hoe") ? "transform" : "infinity");
         packetUtils.playSound(user, Sounds.ENTITY_ENDER_EYE_DEATH, 1f, 0.8f);
     }
 }
