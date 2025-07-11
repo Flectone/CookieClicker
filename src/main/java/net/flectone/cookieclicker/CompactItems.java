ee6723b4e601894e11a9b7860cd0e5525c1a04fe
FunnyBars <realepibarsic@gmail.com>
2025-07-11 14:17:57 +0300

    refactor CompactItems

--- src/main/java/net/flectone/cookieclicker/CompactItems.java
+++ src/main/java/net/flectone/cookieclicker/CompactItems.java
@@ -2,32 +2,37 @@ package net.flectone.cookieclicker;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import net.flectone.cookieclicker.utility.UtilsCookie;
+import net.flectone.cookieclicker.utility.StatsUtils;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import org.bukkit.Bukkit;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Singleton
 public class CompactItems {
-    private final UtilsCookie utilsCookie;
+    private final StatsUtils statsUtils;
+
     @Inject
-    public CompactItems(UtilsCookie utilsCookie) {
-        this.utilsCookie = utilsCookie;
+    public CompactItems(StatsUtils statsUtils) {
+        this.statsUtils = statsUtils;
     }
-    public void compact(net.minecraft.world.entity.player.Inventory inv, ItemStack findItem, ItemStack resultItem, Integer countToFind) {
-        compact(inv, findItem, resultItem, countToFind, 100);
+    public void compact(net.minecraft.world.entity.player.Inventory inv, String findItemTag, ItemStack resultItem, Integer countToFind) {
+        compact(inv, findItemTag, resultItem, countToFind, 100);
     }
+
     public void compact(Inventory inv, ItemStack findItem, ItemStack resultItem, Integer countToFind, Integer times) {
+        compact(inv, statsUtils.getItemTag(findItem), resultItem, countToFind, times);
+    }
+
+    public void compact(Inventory inv, String findItemTag, ItemStack resultItem, Integer countToFind, Integer times) {
         HashMap<Integer, Integer> mapa = new HashMap<>();// Слот : количество предметов
         int itemsCount = 0;
         for (int i = 0; i <= 35; i++) {
             ItemStack itemInSlot = inv.getItem(i);
             if (itemInSlot.getItem() == Items.AIR) continue;
-            if (utilsCookie.compare(itemInSlot, findItem)) {
+            if (statsUtils.hasTag(itemInSlot, findItemTag)) {
                 itemsCount += itemInSlot.getCount();
                 mapa.put(i, itemInSlot.getCount());
             }
--- src/main/java/net/flectone/cookieclicker/events/PacketSetSlotEvent.java
+++ src/main/java/net/flectone/cookieclicker/events/PacketSetSlotEvent.java
@@ -5,31 +5,31 @@ import com.google.inject.Singleton;
 import net.flectone.cookieclicker.CompactItems;
 import net.flectone.cookieclicker.items.ItemManager;
 import net.flectone.cookieclicker.playerdata.ServerCookiePlayer;
-import net.flectone.cookieclicker.utility.ItemTagsUtility;
+import net.flectone.cookieclicker.utility.StatsUtils;
 import net.minecraft.world.entity.player.Player;
 
 @Singleton
 public class PacketSetSlotEvent {
     private final CompactItems compact;
-    private final ItemTagsUtility itemTagsUtility;
-    private final ItemManager manager;
+    private final StatsUtils statsUtils;
+    private final ItemManager loadedItems;
 
     @Inject
-    public PacketSetSlotEvent(CompactItems compact, ItemTagsUtility itemTagsUtility, ItemManager manager) {
+    public PacketSetSlotEvent(CompactItems compact, StatsUtils statsUtils, ItemManager loadedItems) {
         this.compact = compact;
-        this.itemTagsUtility = itemTagsUtility;
-        this.manager = manager;
+        this.statsUtils = statsUtils;
+        this.loadedItems = loadedItems;
     }
 
     public void compactItems(ServerCookiePlayer serverCookiePlayer) {
         Player player = serverCookiePlayer.getPlayer();
 
-        if (itemTagsUtility.getItemTag(player.getOffhandItem()).equals("cookie_crafter")) {
-            compact.compact(player.getInventory(), manager.getNMS("ench_cookie"), manager.getNMS("cookie_block"), 512);
+        if (statsUtils.hasTag(player.getOffhandItem(), "cookie_crafter")) {
+            compact.compact(player.getInventory(), "ench_cookie", loadedItems.getNMS("cookie_block"), 512);
         }
 
-        compact.compact(player.getInventory(), manager.getNMS("cookie"), manager.getNMS("ench_cookie"), 160);
-        compact.compact(player.getInventory(), manager.getNMS("cocoa_beans"), manager.getNMS("ench_cocoa"), 320);
-        compact.compact(player.getInventory(), manager.getNMS("wheat"), manager.getNMS("ench_wheat"), 160);
+        compact.compact(player.getInventory(), "cookie", loadedItems.getNMS("ench_cookie"), 160);
+        compact.compact(player.getInventory(), "cocoa_beans", loadedItems.getNMS("ench_cocoa"), 320);
+        compact.compact(player.getInventory(), "wheat", loadedItems.getNMS("ench_wheat"), 160);
     }
 }
