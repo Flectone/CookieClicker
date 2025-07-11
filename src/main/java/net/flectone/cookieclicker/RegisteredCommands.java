b73a505a7a900a7cf4882f9e7be19b23f5074890
FunnyBars <realepibarsic@gmail.com>
2025-07-11 16:40:14 +0300

    add convert inventory command

--- src/main/java/net/flectone/cookieclicker/RegisteredCommands.java
+++ src/main/java/net/flectone/cookieclicker/RegisteredCommands.java
@@ -16,10 +16,13 @@ import net.flectone.cookieclicker.items.itemstacks.GeneratedCookieItem;
 import net.flectone.cookieclicker.playerdata.ServerCookiePlayer;
 import net.flectone.cookieclicker.utility.CCConversionUtils;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.bukkit.entity.Entity;
 
 import java.util.HashSet;
@@ -49,23 +52,54 @@ public class RegisteredCommands {
         return cookieClicker.build();
     }
 
+    private Player getPlayerExecutor(Entity bukkitExecutor) {
+        if (bukkitExecutor == null)
+            return null;
+
+        return convertUtils.getNMSplayerByUUID(bukkitExecutor.getUniqueId());
+    }
+
+    private ItemStack convertItem(ItemStack originalItem, Integer amount) {
+        GeneratedCookieItem convertedCookieItem = GeneratedCookieItem.fromItemStack(originalItem);
+        return convertedCookieItem.toMinecraftStack().copyWithCount(amount);
+    }
+
+    private void convertInList(NonNullList<ItemStack> items) {
+        for (int i = 0; i < items.size(); i++) {
+            ItemStack singleItem = items.get(i);
+            if (singleItem.getItem() == Items.AIR)
+                continue;
+
+            items.set(i, convertItem(singleItem, singleItem.getCount()));
+        }
+    }
+
     public LiteralCommandNode<CommandSourceStack> createCookieClickerConvert() {
         LiteralArgumentBuilder<CommandSourceStack> convert = Commands.literal("convert")
-                .executes(ctx -> {
-                    Entity bukkitExecutor = ctx.getSource().getExecutor();
-                    if (bukkitExecutor == null)
-                        return 0;
-                    Player nmsExecutor = convertUtils.getNMSplayerByUUID(ctx.getSource().getExecutor().getUniqueId());
-                    nmsExecutor.displayClientMessage(Component.literal("Предмет в вашей руке был переделан"), false);
+                .then(Commands.literal("mainhand")
+                        .executes(ctx -> {
+                            Player nmsExecutor = getPlayerExecutor(ctx.getSource().getExecutor());
+                            if (nmsExecutor == null)
+                                return 0;
+                            nmsExecutor.displayClientMessage(Component.literal("Предмет в вашей руке был переделан"), false);
 
-                    GeneratedCookieItem convertedCookieItem = GeneratedCookieItem.fromItemStack(nmsExecutor.getMainHandItem());
-                    ItemStack covertedMinecraftItem = convertedCookieItem.toMinecraftStack();
-                    covertedMinecraftItem.setCount(nmsExecutor.getMainHandItem().getCount());
+                            nmsExecutor.setItemInHand(InteractionHand.MAIN_HAND, convertItem(nmsExecutor.getMainHandItem(), nmsExecutor.getMainHandItem().getCount()));
 
-                    nmsExecutor.setItemInHand(InteractionHand.MAIN_HAND, covertedMinecraftItem);
+                            return Command.SINGLE_SUCCESS;
+                        }))
+                .then(Commands.literal("inventory")
+                        .executes(ctx -> {
+                            Player nmsExecutor = getPlayerExecutor(ctx.getSource().getExecutor());
+                            if (nmsExecutor == null)
+                                return 0;
 
-                   return Command.SINGLE_SUCCESS;
-                });
+                            Inventory playerInventory = nmsExecutor.getInventory();
+                            convertInList(playerInventory.armor);
+                            convertInList(playerInventory.items);
+                            convertInList(playerInventory.offhand);
+
+                            return Command.SINGLE_SUCCESS;
+                        }));
         return convert.build();
     }
 
@@ -96,59 +130,56 @@ public class RegisteredCommands {
     }
 
     private LiteralArgumentBuilder<CommandSourceStack> createAllEntityCommand() {
-        LiteralArgumentBuilder<CommandSourceStack> allEntities = Commands.literal("all_entities")
+        return Commands.literal("all_entities")
                 .executes(ctx -> {
                     ctx.getSource().getSender().sendMessage("trading_farm: " + packetInteractEvent.getTradingFarm());
                     ctx.getSource().getSender().sendMessage("trading_armorer: " + packetInteractEvent.getTradingArmorer());
                     ctx.getSource().getSender().sendMessage("item_frames: " + packetInteractEvent.getItemFrames());
                     return Command.SINGLE_SUCCESS;
                 });
-        return allEntities;
     }
 
     private LiteralArgumentBuilder<CommandSourceStack> createEntityCommand(String name, boolean register) {
         LiteralArgumentBuilder<CommandSourceStack> villager = Commands.literal("villager");
         RegisteredEntitiesConfig registeredEntitiesConfig = packetInteractEvent.getRegisteredEntitiesConfig();
 
-        villagerTrades.getAllTraders().keySet().forEach(tag -> {
-            villager.then(Commands.literal(tag)
-                            .then(Commands.argument("entity", ArgumentTypes.entity())
-                                    .executes(ctx -> {
-                                        EntitySelectorArgumentResolver entitySelectorArgumentResolver = ctx.getArgument("entity", EntitySelectorArgumentResolver.class);
+        villagerTrades.getAllTraders().keySet().forEach(tag -> villager.then(Commands.literal(tag)
+                .then(Commands.argument("villagerArgument", ArgumentTypes.entity())
+                        .executes(ctx -> {
+                            EntitySelectorArgumentResolver entitySelectorArgumentResolver = ctx.getArgument("villagerArgument", EntitySelectorArgumentResolver.class);
 
-                                        List<Entity> entities = entitySelectorArgumentResolver.resolve(ctx.getSource());
-                                        Entity entity = entities.getFirst();
+                            List<Entity> entities = entitySelectorArgumentResolver.resolve(ctx.getSource());
+                            Entity entity = entities.getFirst();
 
-                                        Set<String> addedEntities = switch (tag) {
-                                            case "trading_farm" -> registeredEntitiesConfig.getVillagers().getFarmers();
-                                            case "trading_armorer" -> registeredEntitiesConfig.getVillagers().getArmorers();
-                                            default -> new HashSet<>();
-                                        };
+                            Set<String> addedEntities = switch (tag) {
+                                case "trading_farm" -> registeredEntitiesConfig.getVillagers().getFarmers();
+                                case "trading_armorer" -> registeredEntitiesConfig.getVillagers().getArmorers();
+                                default -> new HashSet<>();
+                            };
 
-                                        if (register) {
-                                            ctx.getSource().getSender().sendMessage(MiniMessage.miniMessage()
-                                                    .deserialize("registered entity with id " + entity.getEntityId()));
-                                            addedEntities.add(entity.getUniqueId().toString());
-                                        } else {
-                                            if (!addedEntities.contains(entity.getUniqueId().toString()))
-                                                return 0;
-                                            addedEntities.remove(entity.getUniqueId().toString());
-                                            ctx.getSource().getSender().sendMessage(MiniMessage.miniMessage()
-                                                    .deserialize("unregistered entity with id " + entity.getEntityId()));
-                                        }
+                            if (register) {
+                                ctx.getSource().getSender().sendMessage(MiniMessage.miniMessage()
+                                        .deserialize("registered entity with id " + entity.getEntityId()));
+                                addedEntities.add(entity.getUniqueId().toString());
+                            } else {
+                                if (!addedEntities.contains(entity.getUniqueId().toString()))
+                                    return 0;
+                                addedEntities.remove(entity.getUniqueId().toString());
+                                ctx.getSource().getSender().sendMessage(MiniMessage.miniMessage()
+                                        .deserialize("unregistered entity with id " + entity.getEntityId()));
+                            }
 
-                                        registeredEntitiesConfig.save();
+                            registeredEntitiesConfig.save();
 
-                                        return Command.SINGLE_SUCCESS;
-                                    }))
-            );
-        });
+                            return Command.SINGLE_SUCCESS;
+                        }))
+        ));
 
         //Рамка
         LiteralArgumentBuilder<CommandSourceStack> itemFrames = Commands.literal("item_frame");
-        itemFrames.then(Commands.argument("entity", ArgumentTypes.entity())
+        itemFrames.then(Commands.argument("itemFrame", ArgumentTypes.entity())
                 .executes(ctx -> {
-                    EntitySelectorArgumentResolver entitySelectorArgumentResolver = ctx.getArgument("entity", EntitySelectorArgumentResolver.class);
+                    EntitySelectorArgumentResolver entitySelectorArgumentResolver = ctx.getArgument("itemFrame", EntitySelectorArgumentResolver.class);
 
                     List<Entity> entities = entitySelectorArgumentResolver.resolve(ctx.getSource());
                     Entity entity = entities.getFirst();
@@ -177,6 +208,6 @@ public class RegisteredCommands {
         root.then(itemFrames);
 
         return root;
-    };
+    }
 
 }
